17c172c4c7550548f32dd221aae8bb4353e04493
Ram Potham <ram.potham@gmail.com>
2024-04-20 14:59:57 -0400

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -6,31 +6,6 @@ import PIL.Image
 data_path = 'data/subset_data.csv'
 data = pd.read_csv(data_path)
 
-def get_sample(i = -1):
-    # Randomly select a row from the dataset
-    if i == -1:
-        row = data.sample(n=1).iloc[0]
-    else:
-        row = data.iloc[i]
-    # row = data.iloc[4]
-    article_id = row['article_id']
-    img_path = f"./data/images/0{str(article_id)[:2]}/0{article_id}.jpg"
-    if not os.path.exists(img_path):
-        row = data.iloc[0]
-        article_id = row['article_id']
-        img_path = f"./data/images/0{str(article_id)[:2]}/0{article_id}.jpg"
-    image = io.imread(img_path)
-    pil_image = PIL.Image.fromarray(image)
-    return pil_image, row
-
-import pandas as pd
-import os
-import skimage.io as io
-import PIL.Image
-
-data_path = 'data/subset_data.csv'
-data = pd.read_csv(data_path)
-
 def get_sample(i = -1, limit = None):
     if i == -1:
         if limit:
